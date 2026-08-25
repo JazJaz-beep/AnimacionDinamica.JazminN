869694bbc850508420d665cc857c2270c1ea2881
JazminNevarez <jazmin.nevarez@cetys.edu.mx>
2026-08-24 23:26:38 -0700

    Update navegacion.js

--- js/navegacion.js
+++ js/navegacion.js
@@ -3,31 +3,41 @@ const paginas = [
     "A1.html",
     "A2.html",
     "A4.html",
-    "A5.html
+    "A5.html"
 ];
 
+// Obtiene solamente el nombre del archivo actual
 const paginaActual = window.location.pathname.split("/").pop();
 
+// Busca su posición en el arreglo
 const posicion = paginas.indexOf(paginaActual);
 
-const estamosEnActividades =
-    window.location.pathname.includes("/actividades/");
+console.log("Página actual:", paginaActual);
+console.log("Posición:", posicion);
 
 
 // INICIO
-document.getElementById("inicio").href =
-    estamosEnActividades ? "../index.html" : "index.html";
+document.getElementById("inicio").href = "../index.html";
+
+
+// ÚLTIMO
+document.getElementById("ultimo").href =
+    "../actividades/" + paginas[paginas.length - 1];
 
 
 // ANTERIOR
 if (posicion > 0) {
 
-    document.getElementById("anterior").href =
-        estamosEnActividades
-            ? paginas[posicion - 1] === "index.html"
-                ? "../index.html"
-                : paginas[posicion - 1]
-            : "actividades/" + paginas[posicion - 1];
+    if (paginas[posicion - 1] === "index.html") {
+
+        document.getElementById("anterior").href = "../index.html";
+
+    } else {
+
+        document.getElementById("anterior").href =
+            "../actividades/" + paginas[posicion - 1];
+
+    }
 
 } else {
 
@@ -39,20 +49,19 @@ if (posicion > 0) {
 // SIGUIENTE
 if (posicion < paginas.length - 1) {
 
-    document.getElementById("siguiente").href =
-        estamosEnActividades
-            ? paginas[posicion + 1]
-            : "actividades/" + paginas[posicion + 1];
+    if (paginas[posicion + 1] === "index.html") {
+
+        document.getElementById("siguiente").href = "../index.html";
+
+    } else {
+
+        document.getElementById("siguiente").href =
+            "../actividades/" + paginas[posicion + 1];
+
+    }
 
 } else {
 
     document.getElementById("siguiente").style.display = "none";
 
 }
-
-
-// ÚLTIMO
-document.getElementById("ultimo").href =
-    estamosEnActividades
-        ? "A4.html"
-        : "actividades/A4.html";
