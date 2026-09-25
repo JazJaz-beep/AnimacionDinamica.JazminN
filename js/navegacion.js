a78d6e00f3cd3d2b9553bb3f4d0ebba6b34e804c
JazminNevarez <jazmin.nevarez@cetys.edu.mx>
2026-09-25 02:32:16 -0700

    Update navegacion.js

--- js/navegacion.js
+++ js/navegacion.js
@@ -5,7 +5,8 @@ const paginas = [
     "A4.html",
     "A5.html",
     "08-canvasInteractivo.html",
-    "P2 - A1.html"
+    "P2 - A1.html",
+    "p2-a2.html"
 ];
 
 const paginaActual = window.location.pathname.split("/").pop();
